fe462c0028660f76e564b7dd8ca270f63fee9525
Booggs <g.jacquemin63@gmail.com>
2022-02-04 00:10:37 +0100

    ADD stamina bar position and fading

--- 2DPlatformer_Test/Assets/2DPlatformer/Scripts/UI/PlayerHUD/StaminaBarHUDMenu.cs
+++ 2DPlatformer_Test/Assets/2DPlatformer/Scripts/UI/PlayerHUD/StaminaBarHUDMenu.cs
@@ -6,6 +6,7 @@ namespace GSGD2.UI
 	using UnityEngine;
 	using UnityEngine.UI;
 	using GSGD2.Player;
+	using GSGD2.Utilities;
 
 	/// <summary>
 	/// Subsystem that handle the player health bar. It seek player <see cref="Damageable"/>, listen to its <see cref="Damageable.DamageTaken"/>  and <see cref="Damageable.HealthRestored"\> event and react accordingly.
@@ -13,9 +14,16 @@ namespace GSGD2.UI
 	public class StaminaBarHUDMenu : MonoBehaviour
 	{
 		[SerializeField]
-		private Image _healthbarForeground = null;
+		private Image _staminaBarForeground = null;
+
+		[SerializeField]
+		private CanvasGroup _canvasGroup = null;
+
+		[SerializeField]
+		private Timer _fadingTimer;
 
 		private CubeController _cubeController = null;
+		private bool _fadingOut = false;
 
 		private void Awake()
 		{
@@ -24,7 +32,24 @@ namespace GSGD2.UI
 
         private void Update()
         {
-			_healthbarForeground.fillAmount = _cubeController.CurrentStamina;
+			float currentStamina = _cubeController.CurrentStamina;
+			if (currentStamina < 1f && _fadingTimer.IsRunning == false && _canvasGroup.alpha == 0f)
+            {
+				_fadingOut = false;
+				_fadingTimer.Start();
+            }
+			if (currentStamina >= 1f && _fadingTimer.IsRunning == false && _canvasGroup.alpha == 1f)
+            {
+				_fadingOut = true;
+				_fadingTimer.Start();
+			}
+			print(_fadingTimer.IsRunning);
+			if (_fadingTimer.IsRunning)
+				_fadingTimer.Update();
+			if (_fadingOut)
+				_canvasGroup.alpha = 1f - _fadingTimer.Progress;
+			else _canvasGroup.alpha = _fadingTimer.Progress;
+			_staminaBarForeground.fillAmount = currentStamina;
 		}
     }
 }
