a8ef62a7d741bf120a234d59f97a65895dddf566
Booggs <g.jacquemin63@gmail.com>
2022-02-08 23:56:48 +0100

    ADD menus

--- 2DPlatformer_Test/Assets/2DPlatformer/Scripts/Managers/MainMenuManager.cs
+++ 2DPlatformer_Test/Assets/2DPlatformer/Scripts/Managers/MainMenuManager.cs
@@ -11,6 +11,19 @@
 	/// </summary>
 	public class MainMenuManager : MonoBehaviour
 	{
+		[SerializeField]
+		private GameObject _main = null;
+
+		[SerializeField]
+		private GameObject _options = null;
+
+		[SerializeField]
+		private GameObject _keybindings = null;
+
+		[SerializeField]
+		private GameObject _settings = null;
+
+
 		public void Quit()
 		{
 #if UNITY_EDITOR
@@ -18,6 +31,46 @@
 #else
 			Application.Quit();
 #endif //UNITY_EDITOR
+		}
+
+		public void ReturnToMainMenuCanvas()
+        {
+			_main.SetActive(true);
+			_options.SetActive(false);
+			_keybindings.SetActive(false);
+			_settings.SetActive(false);
+		}
+
+		public void ReturnToOptionsCanvas()
+        {
+			_main.SetActive(false);
+			_options.SetActive(true);
+			_keybindings.SetActive(false);
+			_settings.SetActive(false);
+		}
+
+		public void OpenKeybindings()
+        {
+			_main.SetActive(false);
+			_options.SetActive(false);
+			_keybindings.SetActive(true);
+			_settings.SetActive(false);
+		}
+
+		public void OpenSettings()
+        {
+			_main.SetActive(false);
+			_options.SetActive(false);
+			_keybindings.SetActive(false);
+			_settings.SetActive(true);
+		}
+
+		public void OpenOptions()
+		{
+			_main.SetActive(false);
+			_options.SetActive(true);
+			_keybindings.SetActive(false);
+			_settings.SetActive(false);
 		}
 	}
 }
